25d724e60f41f473078ec341382fe5cc95eb8486
K.K. POON <noopkk@gmail.com>
2015-07-03 23:33:16 +0800

    follow react.js to deprecate JST, use Babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: "jsx" }
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
     ]
   }
 };
